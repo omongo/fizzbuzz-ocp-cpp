a841874f8ed6ce4b157b87c4e003a809276496e6
Chanathip Srithanrat <omongo@gmail.com>
2014-06-17 00:14:58 +0700

    Add rule.h, rule.cpp

---
+++ rule.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include "rule.h"
+
+Rule::Rule(std::string syllable) : syllable(syllable) {}
+
+Rule::~Rule() {}
+
+std::string Rule::get_syllable() const {
+  return syllable;
+}
+
+Fizz::Fizz() : Rule("Fizz") {}
+
+bool Fizz::is_valid(unsigned int number) const {
+  return number % 3 == 0;
+}
+
+Buzz::Buzz() : Rule("Buzz") {}
+
+bool Buzz::is_valid(unsigned int number) const {
+  return number % 5 == 0;
+}
---
+++ rule.h
@@ -0,0 +1,27 @@
+#ifndef RULE_H_
+#define RULE_H_
+
+#include <string>
+
+class Rule {
+  std::string syllable;
+public:
+  Rule(std::string);
+  virtual ~Rule();
+  virtual bool is_valid(unsigned int) const = 0;
+  std::string get_syllable() const;
+};
+
+class Fizz : public Rule {
+public:
+  Fizz();
+  bool is_valid(unsigned int) const;
+};
+
+class Buzz : public Rule {
+public:
+  Buzz();
+  bool is_valid(unsigned int) const;
+};
+
+#endif
